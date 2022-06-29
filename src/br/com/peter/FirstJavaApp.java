4b49367116245781b1cc33ac19612ea341cb8ee4
unknown <peterleite.dev@gmail.com>
2022-06-29 09:16:10 -0300

    java first program

---
+++ src/br/com/peter/FirstJavaApp.java
@@ -0,0 +1,15 @@
+package br.com.peter;
+
+import br.com.peter.model.Cat;
+
+public class FirstJavaApp {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        System.out.println(cat);
+
+//        int a = 5;
+//        int b = 3;
+//        System.out.println("Hello World" + (a+b));
+    }
+}
